ad9ae3e7964cc6d43084854cd955a9f3eae40871
dafanf <dafanurulfauziansyah97@gmail.com>
2023-09-17 20:19:52 +0700

    commit first version of solution

---
+++ src/main/java/com/tujuhsembilan/example/controller/SampleController.java
@@ -0,0 +1,31 @@
+package com.tujuhsembilan.example.controller;
+
+// import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/sample")
+public class SampleController {
+
+  @GetMapping("/data-a")
+  @PreAuthorize("hasAuthority('ROLE_A')")
+  public String getDataA() {
+    return "Data A";
+  }
+
+  @GetMapping("/data-b")
+  @PreAuthorize("hasAuthority('ROLE_B')")
+  public String getDataB() {
+    return "Data B";
+  }
+
+  @GetMapping("/data-c")
+  @PreAuthorize("hasAnyAuthority('ROLE_A', 'ROLE_B')")
+  public String getDataC() {
+    return "Data C";
+  }
+
+}
